4fe8a9e8616b8e94c1d86a358a99ebdee4e261dc
ZariZaryab <73702005+ZariZaryab@users.noreply.github.com>
2021-01-15 00:15:35 +0500

    Updated theme
    Changed the UI a bit...

--- COMMANDS/apply.js
+++ COMMANDS/apply.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const config = require("../config.json");
+const q = require("../questions.json");
 
 module.exports.run = async (client, message, args) => {
   
@@ -10,9 +12,9 @@ module.exports.run = async (client, message, args) => {
                         
         return dmEmb;
     }
-    const answers = [];
-    const question =  ['Q1','Q2','Q3','Q4','Q5']
-   
+    
+   const answers = [];
+   const question = Object.values(q)
     
         if(message.channel.type == 'dm') return;
 
@@ -27,42 +29,25 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(startedApp)
        
        const dmStartApp = new Discord.MessageEmbed()
-        .setTitle("🔱 **Let's begin the application**")
-        .setDescription('>Answer the questions below as sent. (one reply per question)')
+        .setTitle("📤 **Let's begin the application**")
+        .setDescription('> Answer the questions below as sent')
         .setColor("RANDOM")
    let appChannel = await message.author.send(dmStartApp);
 
-        await message.author.send(QuestionEmbed(question[0]));
+
+for(let index = 0; index < question.length; index++) {
+  
+  await message.author.send(QuestionEmbed(question[index]));
         let answer = await appChannel.channel.awaitMessages(answer => answer.author.id != client.user.id,  {max: 1});
-        answers[0] = (answer.map(answers => answers.content).join());
+        answers[index] = (answer.map(answers => answers.content).join());
+}
 
-    
-        await message.author.send(QuestionEmbed(question[1]));
-        answer = await appChannel.channel.awaitMessages(answer => answer.author.id != client.user.id,  {max: 1});
-        answers[1] = (answer.map(answers => answers.content).join());
-
-      
-        await message.author.send(QuestionEmbed(question[2]));
-        answer = await appChannel.channel.awaitMessages(answer => answer.author.id != client.user.id,  {max: 1});
-        answers[2] = (answer.map(answers => answers.content).join());
-
-        
-        await message.author.send(QuestionEmbed(question[3]));
-        answer = await appChannel.channel.awaitMessages(answer => answer.author.id != client.user.id,  {max: 1});
-        answers[3] = (answer.map(answers => answers.content).join());
-
-      
-        await message.author.send(QuestionEmbed(question[4]));
-        answer = await appChannel.channel.awaitMessages(answer => answer.author.id != client.user.id,  {max: 1});
-        answers[4] = (answer.map(answers => answers.content).join());
-
-        
         const Embed = new Discord.MessageEmbed()
-        .setAuthor(message.author.username, message.author.displayAvatarURL())
-        .setDescription('Staff can review this application...')
-        .setTitle('New Application Received!')
-        .setFooter(`User Tag: ${message.author.tag} | User ID: ${message.author.id}`)
+        .setAuthor(message.author.username + "'s Application", message.author.displayAvatarURL())
+        .setFooter(`Tag: ${message.author.tag} | ID: ${message.author.id}`)
         .setColor('RANDOM')
+        
+        /* ATTACH TO EMBED */
         question.forEach((q,i)=>{
             Embed.addField(q,answers[i])
         
@@ -84,16 +69,15 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(finishedApp)
        const appFinishDM = new Discord.MessageEmbed()
         .setTitle("✅ **Application has been sent!**")
-        .setDescription('Please be patient while your application is reviewed by staff)
+        .setDescription('Please be patient while your application is reviewed by staff')
         .setThumbnail('https://i.pinimg.com/originals/98/64/9a/98649add72e05e3cc1b8ae0e6f553c8e.gif')
-        .setFooter('Have a good stay!')
+        .setFooter('Good luck!')
         .setColor("RANDOM")
     message.author.send(appFinishDM)
 
-     
-
 
 }
+
 module.exports.help = {
   name: 'apply',
   aliases: ['a']
